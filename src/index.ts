25b85798ce575496ba5b2a92d5cca088da06ee1e
Rob <rob.j.louie@gmail.com>
2022-06-29 17:55:51 -0500

    Update index.ts
    Added comment explaining locking fps vs accounting for intervals with physics/logic.

--- src/index.ts
+++ src/index.ts
@@ -17,11 +17,18 @@ const interval = 1000 / maxFps;
 function update(currentTime: number) {
   const delta = currentTime - previousTime;
 
+  // For simplicity, the frame rate is fixed at 60fps
   if (delta >= interval || !previousTime) {
     previousTime = currentTime - (delta % interval);
 
     drawEngine.context.clearRect(0, 0, drawEngine.width, drawEngine.height);
-    gameStateMachine.getState().onUpdate(currentTime);
+    
+    // Although the game is currently set at 60fps, the state machine accepts a time passed to onUpdate
+    // If you'd like to unlock the framerate, you can instead use an interval passed to onUpdate to 
+    // adjust your physics so they are consistent across all frame rates.
+    // If you do not limit your fps or account for the interval your game will be far too fast or far too 
+    // slow for anyone with a different refresh rate than you.
+    gameStateMachine.getState().onUpdate(delta);
   }
   requestAnimationFrame(update);
 }
